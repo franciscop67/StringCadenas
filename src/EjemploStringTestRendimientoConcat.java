91d27daae0819cc793686082515d3452a6fd2c9a
dev7 <francisco97sp@gmail.com>
2022-06-14 21:07:01 -0500

    cadenas

--- src/EjemploStringTestRendimientoConcat.java
+++ src/EjemploStringTestRendimientoConcat.java
@@ -4,16 +4,22 @@ public class EjemploStringTestRendimientoConcat {
         String b = "b";
         String c = "a";
 
+        StringBuilder sb =  new StringBuilder(a);
 
         long inicio = System.currentTimeMillis();
 
-        for (int i = 0; i < 500; i++){
-            c = c.concat(a).concat(b).concat("\n"); //500 => 2ms
+        for (int i = 0; i < 1000; i++){
+            //c = c.concat(a).concat(b).concat("\n"); //500 => 6ms, 1000 => 7ms
+            c += a + b +"\n"; //500 => 53ms, 1000 => 57ms
+            //sb.append(a).append(b).append("\n"); //500 => 0ms, 1000 => 5ms
+
+
         }
 
         long fin = System.currentTimeMillis();
         System.out.println(fin - inicio);
         System.out.println("c = " + c);
+        System.out.println("sb = " + sb.toString());
 
     }
 }
